19b6ca498004bcb1a542200bb08e3a145f32d5db
Qthinoyy <105048532+Qthinoyy@users.noreply.github.com>
2023-10-30 14:40:10 +0800

    test
    branch

--- serialmain.cpp
+++ serialmain.cpp
@@ -1,12 +1,10 @@
 #include "serialmain.h"
-
-#include <QDebug>
-
 #include "ui_serialmain.h"
+#include <QDebug>
 serialParameter serialparameter;
 #define TEXTOUT_BLACK(QString) ("<font color=\"#000000\">" + QString + "</font>")
 
-serialmain::serialmain(QWidget* parent) : QMainWindow(parent), ui(new Ui::serialmain) {
+serialmain::serialmain(QWidget *parent) : QMainWindow(parent), ui(new Ui::serialmain) {
     ui->setupUi(this);
     serialPort = new QSerialPort;
     connect(ui->button_search, SIGNAL(clicked()), this, SLOT(cfg_searchPorts()), Qt::UniqueConnection);
@@ -15,14 +13,16 @@ serialmain::serialmain(QWidget* parent) : QMainWindow(parent), ui(new Ui::serial
     connect(ui->button_receive, SIGNAL(clicked()), this, SLOT(fun_receive()), Qt::UniqueConnection);
 }
 
-serialmain::~serialmain() { delete ui; }
+serialmain::~serialmain() {
+    delete ui;
+}
 void serialmain::cfg_searchPorts() {
     ui->comboBox_port->clear();
     QList<QString> comList;
-    foreach (const QSerialPortInfo& qspinfo, QSerialPortInfo::availablePorts()) { comList.append(qspinfo.portName()); }
+    foreach (const QSerialPortInfo &qspinfo, QSerialPortInfo::availablePorts()) { comList.append(qspinfo.portName()); }
     QCollator collator;
-    collator.setNumericMode(true);                        // 数字模式
-    std::sort(comList.begin(), comList.end(), collator);  // 使用std::sort函数
+    collator.setNumericMode(true);                       // 数字模式
+    std::sort(comList.begin(), comList.end(), collator); // 使用std::sort函数
     ui->comboBox_port->addItems(comList);
 }
 void serialmain::cfg_setConfig() {
